26c760bc07777829ba820d57da49f1d0d0c26d37
rqmacasieb <reygie.macasieb@research.uwa.edu.au>
2024-12-25 19:07:54 +0800

    some fix

--- examples/michalewicz_10d/mic_lagpy.py
+++ examples/michalewicz_10d/mic_lagpy.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import laGPy
 
-
 # import training dataset
 X = pd.read_csv('mic.dv_pop.csv', header = 0).drop(columns=['real_name']).values #n_tr x n_dv
 y = pd.read_csv('mic.obs_pop.csv', header= 0).drop(columns=['real_name']) #n_tr x 1
