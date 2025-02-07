4e5df36a554fb8d59ebb4939f5473c2c892be2c8
rqmacasieb <reygie.macasieb@research.uwa.edu.au>
2025-02-07 15:12:44 +0800

    few more tests

--- examples/random/build_gp.py
+++ examples/random/build_gp.py
@@ -1,13 +1,16 @@
 import numpy as np
 import laGPy
-
+import time
 
 # Generate example data
-X = np.random.rand(100, 10)
-Z = np.sin(X[:, 0]) + np.cos(X[:, 1]) + 0.1 * np.random.randn(100)
-Xref = np.random.rand(1, 10)
+X = np.random.rand(1000, 20)
+Z = np.sin(X[:, 0]) + np.cos(X[:, 1]) + 0.1 * np.random.randn(1000)
+Xref = np.random.rand(1, 20)
+start_time = time.time()
 
 # Create and fit LaGP model
 gp = laGPy.buildGP(X, Z, verb=1)
 
+elapsed_time = time.time() - start_time
+print(f"Elapsed time to execute buildGP: {elapsed_time:.4f} seconds")
 print("Model successfully built and saved to GPRmodel.gp")
